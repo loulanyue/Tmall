409ad626a9f71445c5f652f28e0ae12990b5e893
loulanyue <260355617@qq.com>
2020-06-13 22:19:48 +0800

    逆向生成微服务代码

--- gulimall-product/src/test/java/com/yfy/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/yfy/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,27 @@
 package com.yfy.gulimall.product;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import com.yfy.gulimall.product.entity.BrandEntity;
+import com.yfy.gulimall.product.service.BrandService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class GulimallProductApplicationTests {
+public class GulimallProductApplicationTests {
+    @Autowired
+    BrandService brandService;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setDescript("华为");
+        brandEntity.setName("华为");
+        brandService.save(brandEntity);
+        System.out.println("保存成功....");
     }
 
 }
