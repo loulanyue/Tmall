a48f4418196a31c1311e551c4d0476ab2c7dde3c
loulanyue <260355617@qq.com>
2020-06-21 23:52:11 +0800

    添加第三方服务OSS

---
+++ gulimall-common/src/main/java/com/yfy/common/valid/UpdateGroup.java
@@ -0,0 +1,4 @@
+package com.yfy.common.valid;
+
+public interface UpdateGroup {
+}
---
+++ gulimall-third-party/src/test/java/com/yfy/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -0,0 +1,11 @@
+package com.yfy.gulimall.thirdparty;
+
+import org.junit.Test;
+
+public class GulimallThirdPartyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
